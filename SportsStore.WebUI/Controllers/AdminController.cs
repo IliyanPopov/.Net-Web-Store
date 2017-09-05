deee2126c90f9dbc299bc0878d9ca975052d2410
iliyan <iliyan.popov93@gmail.com>
2017-09-05 20:22:37 +0300

    Admin controller added

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsStore.WebUI.Controllers
+{
+    using System.Web.Mvc;
+    using Data.Contracts;
+    using SportsStore.Models.Entities;
+
+    public class AdminController : Controller
+    {
+        private IGenericRepository<Product> _products;
+
+        public AdminController(IGenericRepository<Product> products)
+        {
+            this._products = products;
+        }
+
+        public ViewResult Index()
+        {
+            return View(this._products.All);
+        }
+    }
+}
