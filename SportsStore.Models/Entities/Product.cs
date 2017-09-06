c174a37d7f57eec4fa689ef816a9325f8e413a33
iliyan <iliyan.popov93@gmail.com>
2017-09-06 19:44:58 +0300

    Model validation added

--- SportsStore.Models/Entities/Product.cs
+++ SportsStore.Models/Entities/Product.cs
@@ -3,18 +3,23 @@
     using System.ComponentModel.DataAnnotations;
     using System.Web.Mvc;
 
-    public class Product 
+    public class Product
     {
         [HiddenInput(DisplayValue = false)]
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
 
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
 
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "Please specify a category")]
         public int CategoryId { get; set; }
 
         public Category Category { get; set; }
