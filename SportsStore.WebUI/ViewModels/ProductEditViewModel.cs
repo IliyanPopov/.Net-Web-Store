1f4b1152e172779ed436074392a1cd46901ae219
iliyan <iliyan.popov93@gmail.com>
2017-09-07 16:37:54 +0300

    client validation added to admin edit product

--- SportsStore.WebUI/ViewModels/ProductEditViewModel.cs
+++ SportsStore.WebUI/ViewModels/ProductEditViewModel.cs
@@ -1,18 +1,24 @@
 ﻿namespace SportsStore.WebUI.ViewModels
 {
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using SportsStore.Models.Entities;
 
     public class ProductEditViewModel
     {
+        [Required(ErrorMessage = "Please specify a category")]
         public int CategoryId { get; set; }
 
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
 
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
 
 
